65dc67ec52471233ff236452c56a77cca424539b
Nick Popov <np201585@gmail.com>
2022-08-27 20:46:40 +0300

    Лекция 2

---
+++ Example10_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 1, 2, 3, 18, 5, 6, 7, 18 };
+int n = array.Length;
+int find = 18;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
